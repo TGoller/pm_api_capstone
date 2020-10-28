f0002f481106484bc29cfde82e87899f76651a3c
Tracy <tracy.goller@libertymutual.com>
2020-10-28 15:14:53 -0400

    Midday Wednesday

--- controllers/policyController.js
+++ controllers/policyController.js
@@ -2,6 +2,10 @@ const Policy = require('../models/policy');
 
 // Handle Policy create on POST.
 exports.policy_create_post = function (req, res) {
+    if (!req.body.name) {
+        res.status(400).send("No name included on the request")
+    }
+
     const policy = new Policy(req.body);
     policy.save()
         .then(data => {
@@ -10,7 +14,6 @@ exports.policy_create_post = function (req, res) {
         .catch(err => {
             res.status(400).send('There was an issue saving the policy');
         });
-    
 };
 
 // Handle Policy list on GET.
@@ -22,5 +25,60 @@ exports.policy_list_get = function (req, res) {
         .catch(err => {
             res.status(400).send('There was an issue getting the policies');
         });
+
+};
+
+// Handle Policy get a specific policy.
+exports.getByID = (req, res) => {
+    const id = req.params.id;
     
+    Policy.findById(id)
+        .then(data => {
+            if (!data) {
+                res.status(400).send(`No policy found with id: ${id}`);
+            }
+            else {
+                res.status(200).send(data);
+            }
+        })
+        .catch(err => {
+            res.status(400).send('There was an issue finding that one policy');
+        });
+};
+
+
+// update Policy by ID
+exports.updateByID = (req, res) => {
+    const id = req.params.id;
+    
+    Policy.findOneAndUpdate(id,req.body,{new:true})
+        .then(data => {
+            if (!data) {
+                res.status(400).send(`No policy found with id: ${id}`);
+            }
+            else {
+                res.status(200).send(data);
+            }
+        })
+        .catch(err => {
+            res.status(400).send('There was an issue finding that one policy');
+        });
+};
+
+// deleteByID
+exports.deleteByID = (req, res) => {
+    const id = req.params.id;
+    
+    Policy.findByIdAndDelete(id)
+        .then(data => {
+            if (!data) {
+                res.status(400).send(`No policy found with id: ${id}`);
+            }
+            else {
+                res.status(200).send(`${id} Policy deleted`);
+            }
+        })
+        .catch(err => {
+            res.status(400).send('There was an issue finding that one policy');
+        });
 };
--- routes/policyRouter.js
+++ routes/policyRouter.js
@@ -13,4 +13,14 @@ router.post('/', policy_controller.policy_create_post);
 // GET request for listing all policies.
 router.get('/', policy_controller.policy_list_get);
 
+// GET a policy with a specific ID
+router.get('/:id', policy_controller.getByID);
+
+// PUT a policy with a specific ID
+router.put('/:id', policy_controller.updateByID);
+
+// Delete a policy with a specific ID
+router.delete('/:id', policy_controller.deleteByID);
+
+
 module.exports = router;
